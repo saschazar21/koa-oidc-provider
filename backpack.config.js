0cac12c21392c6e8a05198289c3dad577d934ae3
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-06-02 16:04:46 +0200

    feat(backpack): Added dotenv plugin to webpack

--- backpack.config.js
+++ backpack.config.js
@@ -1,6 +1,31 @@
+const DotEnv = require('dotenv-webpack');
+const { resolve } = require('path');
+
 module.exports = {
   webpack: (config, options, webpack) => {
-    config.entry.main = './server/index.js'
-    return config
-  }
-}
+    const conf = config;
+    conf.entry.main = './server/index.js';
+
+    const env = new DotEnv({
+      path: resolve(process.cwd(), './.env'),
+      safe: resolve(process.cwd(), './.env.sample'),
+      systemvars: true,
+    });
+
+    const idx = conf.plugins.findIndex(el => el.hasOwnProperty('definitions'));
+
+    if (idx < 0) {
+      conf.plugins = [
+        ...conf.plugins,
+        env,
+      ];
+    } else {
+      conf.plugins[idx].definitions = {
+        ...conf.plugins[idx].definitions,
+        ...env.definitions,
+      };
+    }
+
+    return conf;
+  },
+};
