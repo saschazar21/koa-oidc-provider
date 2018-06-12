10982b1523db3d98366453faf1001638002cc3c3
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-06-12 15:55:03 +0200

    fix(tests): Split server tests into modules, removed nuxt part

--- test/server/index.js
+++ test/server/index.js
@@ -1,3 +1,44 @@
 /* eslint-env node, mocha */
 /* eslint prefer-arrow-callback: [ "off", { "allowNamedFunctions": true } ] */
 /* eslint func-names: ["off", "always"] */
+import chai from 'chai';
+import http from 'chai-http';
+import Koa from 'koa';
+import mount from 'koa-mount';
+
+import Configuration from '../../server/lib/config';
+import bootstrapProvider from '../../server/provider';
+
+chai.use(http);
+
+after(async function () {
+  this.request.close();
+  this.app.close();
+});
+
+before(async function () {
+  const koa = new Koa();
+  this.config = new Configuration();
+  this.provider = await bootstrapProvider();
+  koa.use(mount(this.provider));
+  this.app = koa.listen();
+  this.request = chai.request(this.app).keepOpen();
+});
+
+describe('OIDC Provider', function () {
+  it('should provide a route for /.well-known/openid-configuration', async function () {
+    const res = await this.request.get('/.well-known/openid-configuration');
+    chai.expect(res).to.have.status(200);
+    chai.expect(res).to.have.header('content-type');
+    chai.expect(res.type).to.equal('application/json');
+  });
+
+  it('should provide a route for JWKs', async function () {
+    const config = await this.config.getConfig();
+    const url = config.routes && config.routes.certificates ? config.routes.certificates : '/certs';
+    const res = await this.request.get(url);
+    chai.expect(res).to.have.status(200);
+    chai.expect(res).to.have.header('content-type');
+    chai.expect(res.type).to.equal('application/json');
+  });
+});
--- test/test.js
+++ test/test.js
@@ -3,47 +3,29 @@
 /* eslint func-names: ["off", "always"] */
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import Koa from 'koa';
 
-import Configuration from '../server/lib/config';
-import { start } from '../server/server';
+import router from '../server/routes';
 
 chai.use(chaiHttp);
 
 describe('Server', function () {
-  this.timeout(15000);
-
   before(async function () {
-    this.config = new Configuration();
-    this.app = await start();
+    const koa = new Koa();
+    koa.use(router.routes());
+    this.app = koa.listen();
     this.request = chai.request(this.app).keepOpen();
   });
 
   after(async function () {
     this.request.close();
+    this.app.close();
   });
 
   describe('Base', function () {
     it('should redirect to /web', async function () {
       const res = await this.request.get('/');
       return chai.expect(res).to.redirect;
-    });
-  });
-
-  describe('OIDC Provider', function () {
-    it('should provide a route for /.well-known/openid-configuration', async function () {
-      const res = await this.request.get('/.well-known/openid-configuration');
-      chai.expect(res).to.have.status(200);
-      chai.expect(res).to.have.header('content-type');
-      chai.expect(res.type).to.equal('application/json');
-    });
-
-    it('should provide a route for JWKs', async function () {
-      const config = await this.config.getConfig();
-      const url = config.routes && config.routes.certificates ? config.routes.certificates : '/certs';
-      const res = await this.request.get(url);
-      chai.expect(res).to.have.status(200);
-      chai.expect(res).to.have.header('content-type');
-      chai.expect(res.type).to.equal('application/json');
     });
   });
 });
