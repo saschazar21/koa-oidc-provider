3729cfaf3069bbd094bcfc148d799e519d8f97f1
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-06-12 19:42:04 +0200

    fix(tests): Rewrote Nuxt.js test

--- test/server/nuxt.js
+++ test/server/nuxt.js
@@ -1,33 +1,35 @@
 /* eslint-env node, mocha */
 /* eslint prefer-arrow-callback: [ "off", { "allowNamedFunctions": true } ] */
 /* eslint func-names: ["off", "always"] */
-// import chai from 'chai';
-// import http from 'chai-http';
-// import { Nuxt, Builder } from 'nuxt';
+import chai from 'chai';
+import http from 'chai-http';
+import { Nuxt, Builder } from 'nuxt';
 
-// import config from '../../nuxt.config';
+import config from '../../nuxt.config';
 
-// chai.use(http);
-// const PORT = 49935;
+chai.use(http);
 
-// describe('Nuxt.js', function () {
-//   this.timeout(15000);
-//   before(async function () {
-//     this.nuxt = new Nuxt(config);
-//     await new Builder(this.nuxt).build();
-//     await this.nuxt.listen(PORT, 'localhost');
-//     this.request = chai.request(`http://localhost:${PORT}`).keepOpen();
-//   });
+describe('Nuxt.js', function () {
+  this.timeout(15000);
+  before(async function () {
+    this.nuxt = new Nuxt(config);
+    await new Builder(this.nuxt).build();
+    this.app = this.nuxt.render.listen();
+    this.request = chai.request(this.app).keepOpen();
+  });
 
-//   after(async function () {
-//     await this.nuxt.close(this.request.close.bind(this));
-//   });
+  after(function (done) {
+    this.request.close();
+    this.app.close();
+    this.nuxt.close()
+      .then(done);
+  });
 
-//   describe('Application', function () {
-//     it('should resolve /web/login as HTML', async function () {
-//       const res = await this.request.get('/web/login');
-//       chai.expect(res).to.have.status(200);
-//       chai.expect(res.type).to.equal('text/html');
-//     });
-//   });
-// });
+  describe('Application', function () {
+    it('should resolve /web/login as HTML', async function () {
+      const res = await this.request.get('/web/login');
+      chai.expect(res).to.have.status(200);
+      chai.expect(res.type).to.equal('text/html');
+    });
+  });
+});
