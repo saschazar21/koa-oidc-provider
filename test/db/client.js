675229e9660dce25f0826cf5fe49f7e7922af504
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-06-25 21:38:14 +0200

    feat(mongo): Enhanced mongo tests

---
+++ test/db/client.js
@@ -0,0 +1,57 @@
+import dotenv from 'dotenv';
+import test from 'ava';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+import { configuration, initMongo } from '../../server/lib/db/mongo';
+import clientModel from '../../server/lib/db/models/client';
+import { safeIdFactory } from '../../server/lib/tools/id';
+
+chai.use(chaiAsPromised);
+
+dotenv.config();
+let Client;
+let client;
+let mongoose;
+
+test.before(async () => {
+  const config = {
+    ...configuration,
+    pass: process.env.MONGO_PASSWORD,
+    user: process.env.MONGO_USER,
+  };
+
+  mongoose = await initMongo(process.env.MONGO_HOST, process.env.MONGO_PORT, config);
+  Client = await clientModel(mongoose);
+});
+
+test.serial('should create a client model', async () => {
+  client = new Client({
+    owner: safeIdFactory(),
+    redirect_uris: 'https://localme.com',
+    client_name: 'Pizzabot',
+  });
+  chai.expect(client.toJSON()).to.haveOwnProperty('_id');
+  chai.expect(await client.save()).to.have.property('_id');
+});
+
+test.serial('should delete the client model', async () => {
+  const deleted = await Client.deleteOne({ client_name: 'Pizzabot' });
+  chai.expect(deleted.n).to.be.greaterThan(0);
+});
+
+test('should fail with dulicate client_name', async () => {
+  const newClient = {
+    ...client,
+  };
+  chai.expect(new Client(newClient).save()).to.be.rejectedWith(Error);
+});
+
+test('should fail with redirect_uris set to localhost', async () => {
+  const newClient = {
+    ...client,
+    client_name: 'Kebabbot',
+    redirect_uris: 'https://localhost',
+  };
+  chai.expect(new Client(newClient).save()).to.be.rejectedWith(Error);
+});
--- test/db/user.js
+++ test/db/user.js
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 import test from 'ava';
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 
 import { configuration, initMongo } from '../../server/lib/db/mongo';
 import userModel from '../../server/lib/db/models/user';
+
+chai.use(chaiAsPromised);
 
 dotenv.config();
 let connection;
@@ -18,7 +21,7 @@ test.serial('should connect to MongoDB', async () => {
   };
   connection = await initMongo(process.env.MONGO_HOST, process.env.MONGO_PORT, config);
   User = await userModel(connection);
-  expect(typeof connection).to.equal('object');
+  chai.expect(typeof connection).to.equal('object');
 });
 
 test.serial('should create a user model and save to DB', async () => {
@@ -32,14 +35,30 @@ test.serial('should create a user model and save to DB', async () => {
 });
 
 test.serial('should check if hashed Password is correct', async () => {
-  expect(await user.correctPassword('somepassword')).to.equal(true);
+  chai.expect(await user.correctPassword('somepassword')).to.equal(true);
 });
 
 test.serial('should delete the user model from DB', async () => {
   const u = user.toJSON();
   // eslint-disable-next-line no-underscore-dangle
   const result = await User.findById(u._id);
-  expect(result.toJSON()).to.deep.equal(u);
+  chai.expect(result.toJSON()).to.deep.equal(u);
   // eslint-disable-next-line no-underscore-dangle
   return User.deleteOne({ _id: u._id });
+});
+
+test('should fail when providing no given_name', async () => {
+  const newUser = {
+    ...user.toJSON(),
+    given_name: null,
+  };
+  return chai.expect(new User(newUser).save()).to.be.rejectedWith(Error);
+});
+
+test('should fail when providing malformed e-mail', async () => {
+  const newUser = {
+    ...user.toJSON(),
+    email: 'johndoe',
+  };
+  return chai.expect(new User(newUser).save()).to.be.rejectedWith(Error);
 });
