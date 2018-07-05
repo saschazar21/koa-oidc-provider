03cbb8ee13bc82f8d4a002c005bbdf852f10c910
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-07-05 20:24:59 +0200

    fix(provider): Enhanced interaction routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,16 +3,39 @@ import Router from 'koa-router';
 import * as url from '../lib/tools/url';
 
 const info = debug('info');
+const error = debug('error');
 const router = new Router();
 
 export default async function bootstrapRoutes(provider) {
-  router.get(`${url.oidcPrefix}/interaction/:id`, async (ctx) => {
-    const details = await provider.interactionDetails(ctx.req);
-    info(details);
-    // TODO: Handle details
+  router.get(`${url.oidcPrefix}/interaction/:grant`, async (ctx) => {
+    try {
+      const details = await provider.interactionDetails(ctx.req);
+      info(details);
 
-    ctx.redirect(`${url.nuxtPrefix}/interaction/${ctx.params.id}`);
-    ctx.status = 301;
+      const redirect = details.interaction.error === 'login_required'
+        ? `${url.nuxtPrefix}/login?client_id=${details.params.client_id}&grant=${ctx.params.grant}`
+        : `${url.nuxtPrefix}/interaction/${ctx.params.grant}?client_id=${details.params.client_id}`;
+      ctx.redirect(redirect);
+    } catch (e) {
+      error(e.message || e);
+      ctx.redirect(`${url.nuxtPrefix}/error?status=${e.status || 500}`);
+    }
+    ctx.status = 302;
+  });
+
+  router.post(`${url.oidcPrefix}/interaction/:grant/confirm`, async (ctx, next) => {
+    const result = {};
+    await provider.interactionFinished(ctx.req, ctx.res, result);
+    await next();
+  });
+
+  router.post(`${url.oidcPrefix}/interaction/:grant/login`, async (ctx) => {
+    // TODO: Read user's accountId and store it in session
+    await provider.setProviderSession(ctx.req, ctx.res, {
+      account: 'user accountId',
+    });
+    ctx.redirect(`${url.oidcPrefix}/interaction/${ctx.params.grant}`);
+    ctx.status = 302;
   });
 
   router.all('/', (ctx) => {
