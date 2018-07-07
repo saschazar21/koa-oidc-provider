f84ab6864e9d9abfdee4af2e4f4ec848be08d183
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-07-07 13:18:21 +0200

    fix(tests): Enhanced provider test w/ client

--- test/server/provider.js
+++ test/server/provider.js
@@ -2,21 +2,25 @@ import dotenv from 'dotenv';
 import test from 'ava';
 import chai from 'chai';
 import http from 'chai-http';
+import chaiAsPromised from 'chai-as-promised';
 import Koa from 'koa';
 import mount from 'koa-mount';
 
 import Configuration from '../../server/lib/config';
 import bootstrapProvider from '../../server/provider';
+import { getBaseClient } from '../../server/lib/config/clients';
 
 chai.use(http);
+chai.use(chaiAsPromised);
 dotenv.config();
 
 let config;
 let app;
+let provider;
 let request;
 
 test.before(async () => {
-  const provider = await bootstrapProvider();
+  provider = await bootstrapProvider();
 
   const koa = new Koa();
   config = new Configuration();
@@ -44,4 +48,10 @@ test('Provider should provide a route for JWKs', async () => {
   chai.expect(res).to.have.status(200);
   chai.expect(res).to.have.header('content-type');
   chai.expect(res.type).to.equal('application/json');
+});
+
+test('Provider should contain base client', async () => {
+  const baseClient = await getBaseClient();
+  const realClient = await provider.Client.find(baseClient.client_id);
+  chai.expect(baseClient.client_id).to.equal(realClient.clientId);
 });
