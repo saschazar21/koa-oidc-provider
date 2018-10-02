46b8860502e4c9ad8fc81160001cceaebe76a3e1
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-10-02 18:17:27 +0200

    feat(auth): Added logout route & mechanism

--- components/nav-list.vue
+++ components/nav-list.vue
@@ -2,7 +2,7 @@
   <ul class="list--inline list--blank">
     <li><nuxt-link to="/">Home</nuxt-link></li>
     <li v-if="!user"><a href="/login">Login</a></li>
-    <li v-if="user"><nuxt-link to="/logout">Logout</nuxt-link></li>
+    <li v-if="user"><a href="/logout">Logout</a></li>
     <li><strong><a href="https://sascha.work">sascha.work</a></strong></li>
   </ul>
 </template>
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,43 @@
 import Promise from 'bluebird';
 import Router from 'koa-router';
+import debug from 'debug';
 
 import apiRoutes from './api';
 import authRoutes from './auth';
 import oidcRoutes from './oidc';
+import bootstrapProvider from '../provider';
 import { nuxtPrefix } from '../lib/tools/url';
 
 const router = new Router();
+const error = debug('error:auth');
+const info = debug('info');
 
 export default async function bootstrapRoutes(customClient) {
   const result = await Promise.all([
     apiRoutes(customClient),
     authRoutes(customClient),
     oidcRoutes(customClient),
+    bootstrapProvider(customClient),
   ]);
+  const provider = result.pop();
 
   result.forEach(route => router.use(route.routes()));
+
+  router.get('/logout', async (ctx) => {
+    if (ctx.state && ctx.state.user) {
+      const { user } = ctx.state;
+      try {
+        const token = await provider.AccessToken.find(user.token.access_token);
+        await token.destroy();
+        ctx.logout();
+        /* eslint-disable-next-line no-underscore-dangle */
+        info(`Token ${token._id} destroyed, ${user.name} logged out.`);
+      } catch (e) {
+        error(e.message || e);
+      }
+    }
+    return ctx.redirect('/');
+  });
 
   router.all('/', (ctx) => {
     ctx.redirect(nuxtPrefix);
