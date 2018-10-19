5c28f0457e5fdd062000f6e2115d109f76134c6d
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-10-19 21:48:32 +0200

    fix(server): Fixed client API routes

--- server/routes/api/clients.js
+++ server/routes/api/clients.js
@@ -33,55 +33,89 @@ export default async function clientRoutes(customClient) {
       }
     },
   );
-  // Don't use POST /, as this is default for dynamic client registration!
 
-  router.get('/:id/reset', async (ctx) => {
-    try {
-      const result = await adapter.reset(ctx.params.id);
-      if (!result) {
-        throw new Error('Something went wrong while resetting the password.');
+  router.get(
+    '/:id/reset',
+    passport.authenticate(['bearer']),
+    async (ctx, next) => requireScopes(ctx, next, ['client', 'client:edit']),
+    async (ctx) => {
+      try {
+        const result = await adapter.reset(ctx.params.id);
+        if (!result) {
+          throw new Error('Something went wrong while resetting the password.');
+        }
+        ctx.status = 200;
+        ctx.body = result.toJSON();
+      } catch (e) {
+        error(e.message || e);
+        ctx.status = e.statusCode || 500;
+        ctx.body = { error: e.message || e };
       }
-      ctx.status = 200;
-      ctx.body = result.toJSON();
-    } catch (e) {
-      error(e.message || e);
-      ctx.status = e.statusCode || 500;
-      ctx.body = { error: e.message || e };
-    }
-  });
+    },
+  );
 
-  router.put('/:id', async (ctx) => {
-    if (!ctx.req.body || ctx.req.body.client_secret) {
-      const err = ctx.req.body ? 'Updating client secret is not allowed' : 'No request body found.';
-      error(err);
-      ctx.status = 400;
-      ctx.body = { error: err };
-    }
-    try {
-      const result = await adapter.upsert(ctx.params.id, ctx.req.body);
-      ctx.status = 200;
-      ctx.body = result.toJSON();
-    } catch (e) {
-      error(e.message || e);
-      ctx.status = e.statusCode || 500;
-      ctx.body = { error: e.message || e };
-    }
-  });
-
-  router.delete('/:id', async (ctx) => {
-    try {
-      const result = await adapter.destroy(ctx.params.id);
-      if (!result) {
-        throw new Error(`Something went wrong while deleting client ID: ${ctx.params.id}`);
+  router.post(
+    '/',
+    passport.authenticate(['bearer']),
+    async (ctx, next) => requireScopes(ctx, next, ['client', 'client:create']),
+    async (ctx) => {
+      try {
+        if (!ctx.req.body || Object.keys(ctx.req.body).length === 0) {
+          throw new Error('Empty body is not supported!');
+        }
+        const result = await adapter.upsert(null, ctx.req.body);
+        ctx.status = 200;
+        ctx.body = result.toJSON();
+      } catch (e) {
+        error(e.message || e);
+        ctx.status = e.statusCode || 500;
+        ctx.body = { error: e.message || e };
       }
-      ctx.status = 200;
-      ctx.body = result.toJSON();
-    } catch (e) {
-      error(e.message || e);
-      ctx.status = e.statusCode || 500;
-      ctx.body = { error: e.message || e };
-    }
-  });
+    },
+  );
+
+  router.put(
+    '/:id',
+    passport.authenticate(['bearer']),
+    async (ctx, next) => requireScopes(ctx, next, ['client', 'client:edit']),
+    async (ctx) => {
+      if (!ctx.req.body || ctx.req.body.client_secret) {
+        const err = ctx.req.body ? 'Updating client secret is not allowed' : 'No request body found.';
+        error(err);
+        ctx.status = 400;
+        ctx.body = { error: err };
+      }
+      try {
+        const result = await adapter.upsert(ctx.params.id, ctx.req.body);
+        ctx.status = 200;
+        ctx.body = result.toJSON();
+      } catch (e) {
+        error(e.message || e);
+        ctx.status = e.statusCode || 500;
+        ctx.body = { error: e.message || e };
+      }
+    },
+  );
+
+  router.delete(
+    '/:id',
+    passport.authenticate(['bearer']),
+    async (ctx, next) => requireScopes(ctx, next, ['client', 'client:delete']),
+    async (ctx) => {
+      try {
+        const result = await adapter.destroy(ctx.params.id);
+        if (!result) {
+          throw new Error(`Something went wrong while deleting client ID: ${ctx.params.id}`);
+        }
+        ctx.status = 200;
+        ctx.body = result.toJSON();
+      } catch (e) {
+        error(e.message || e);
+        ctx.status = e.statusCode || 500;
+        ctx.body = { error: e.message || e };
+      }
+    },
+  );
 
   return router;
 }
